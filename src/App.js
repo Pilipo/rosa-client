395a912e2a500a4da7399fe121c751aaf0a25ecf
Phillip Lehner <pl@philliplehner.com>
2021-03-05 14:24:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import NavBar from './components/NavBar';
+import Appbar from './components/Appbar';
+import Recipes from './components/Recipes';
+import CreateRecipe from './components/Recipes/CreateRecipe';
 import './App.css';
 
 function App() {
+  const [recipeIn, setRecipeIn] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Appbar
+        onSearchClick={() => setShowSearch(!showSearch)}
+        onAddRecipeClick={() => setRecipeIn(true)}
+      />
+      <div className="content-holder">
+        {recipeIn
+          ? <CreateRecipe onClick={() => setRecipeIn(false)} />
+          : <Recipes />}
+      </div>
     </div>
   );
 }
